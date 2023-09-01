889b1ca79c03b1cd1057b95ec7c9cfd2b4a8f591
Diego <cortesroadiegoalejandro@gmail.com>
2023-09-01 15:58:48 -0500

    develop: Add headless

--- src/test/java/core/FrameworkConfig.java
+++ src/test/java/core/FrameworkConfig.java
@@ -3,4 +3,5 @@ package core;
 public abstract class FrameworkConfig {
     public static final String BASE_URL = "https://www.saucedemo.com/";
     public static final long ELEMENT_TIMEOUT = 5000;
+    public static final boolean HEADLESS = true;
 }
