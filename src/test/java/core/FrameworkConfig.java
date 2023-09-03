32ece96979505a0e3209951a2970a8bdffa7927b
Diego <cortesroadiegoalejandro@gmail.com>
2023-09-03 17:17:44 -0500

    develop: Update Azure Devops Pipeline

--- src/test/java/core/FrameworkConfig.java
+++ src/test/java/core/FrameworkConfig.java
@@ -2,6 +2,6 @@ package core;
 
 public abstract class FrameworkConfig {
     public static final String BASE_URL = "https://www.saucedemo.com/";
-    public static final long ELEMENT_TIMEOUT = 10000;
+    public static final long ELEMENT_TIMEOUT = 15000;
     public static final boolean HEADLESS = true;
 }
