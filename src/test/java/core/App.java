cd5925408b4b219f25bad082dccebe37726fc85e
Diego <cortesroadiegoalejandro@gmail.com>
2023-09-13 21:19:03 -0500

    develop: Update Jenkins pipeline

--- src/test/java/core/App.java
+++ src/test/java/core/App.java
@@ -4,5 +4,5 @@ public abstract class App {
     public static final String BASE_URL = "https://www.saucedemo.com/";
     public static final long ELEMENT_TIMEOUT = 15000;
     public static final boolean HEADLESS = true;
-    public static final String PLATFORM = "remote";
+    public static final String PLATFORM = "local";
 }
